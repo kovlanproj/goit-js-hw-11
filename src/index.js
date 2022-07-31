5eff8effa577fdaa4bff2027f7c93583f0ef21de
kovlanproj <kozorizva@gmail.com>
2022-07-31 23:45:00 +0300

    gallery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import { getPhotos } from './js/pixabayapi';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-getPhotos();
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+
+var lightbox = new SimpleLightbox('.gallery a');
+
+form.addEventListener('submit', onFormSearch);
+
+function makeImageMarkup({
+  largeImageURL,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `
+  
+    <div class="gallery__item">
+    <a href="${largeImageURL}" class="gallery__link">
+  <img src="${webformatURL}" alt="${tags}"  loading="lazy" class="gallery__image" /></a>
+  <ul class="gallery__info">
+    <li class="info-item">
+      <p><b>Likes</b>
+      <p>${likes}
+    </li>
+    <li class="info-item">
+      <p><b>Views</b>
+      <p>${views}
+    </li>
+    <li class="info-item">
+     <p> <b>Comments</b>
+     <p> ${comments}
+    </li>
+    <li class="info-item">
+     <p> <b>Downloads</b>
+     <p> ${downloads}
+    </li>
+  </ul>
+  
+</div>
+
+    `;
+}
+
+function onFormSearch(e) {
+  e.preventDefault();
+
+  getPhotos(form.searchQuery.value, '1').then(images => {
+    addMarkup(gallery, images);
+  });
+
+  console.log(lightbox);
+}
+
+function makeGalleryMarkup(images) {
+  return images.map(image => makeImageMarkup(image)).join('');
+}
+
+function addMarkup(placeRefForMarkup, galleryItems) {
+  placeRefForMarkup.insertAdjacentHTML(
+    'beforeend',
+    makeGalleryMarkup(galleryItems)
+  );
+  lightbox.refresh();
+}
--- src/js/pixabayapi.js
+++ src/js/pixabayapi.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
-const API_KEY = 28916923 - d414eade46dc2c6542ccb3cb8;
+const API_KEY = '28916923-d414eade46dc2c6542ccb3cb8';
+const BASE_URL = 'https://pixabay.com/api/';
 
-export const getPhotos = async searchQuery => {
+export const getPhotos = async (searchQuery, page) => {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo`
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=10`
     );
-    console.log(response);
+    return response.data.hits;
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
