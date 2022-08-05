3f77a912a5ef3c284049c7a647da051774d366e4
kovlanproj <kozorizva@gmail.com>
2022-08-05 15:33:22 +0300

    done

--- src/js/image-service.js
+++ src/js/image-service.js
@@ -2,11 +2,31 @@ const API_KEY = '28916923-d414eade46dc2c6542ccb3cb8';
 const BASE_URL = 'https://pixabay.com/api/';
 const axios = require('axios');
 
-export default class imageApiService {
+export default class ImageApiService {
+  constructor() {
     this.searchQuery = '';
     this.page = 1;
-}
+  }
 
-fetchImages() {
-    
+  async fetchImages() {
+    const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
